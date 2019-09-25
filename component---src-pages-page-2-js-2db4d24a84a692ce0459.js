8bf569d4c4cc20807ef52e46eb46eaf657c3f309
Walter Cruz <waltercruz@gmail.com>
2019-09-25 01:08:37 -0300

    Updates

---
+++ component---src-pages-page-2-js-2db4d24a84a692ce0459.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{207:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(130),c=a(204),r=a(205);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-2db4d24a84a692ce0459.js.map
