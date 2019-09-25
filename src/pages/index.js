07a4a0b80b4ec9a474926f6a5681c53173c90c5e
Walter Cruz <waltercruz@gmail.com>
2019-09-25 00:57:37 -0300

    links

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,8 @@ const IndexPage = () => (
   <Layout>
     <img src={'headshot.jpg'} alt="Waltin" class="head" />
     <h1>Walter Cruz</h1>
-    <a href="https://stoyca.com.br"><Button variant="contained" color="primary">Stoyca</Button></a>
+    <a href="http://bit.ly/stoycamusic"><Button variant="contained" color="primary">Stoyca</Button></a>
+    <a href="http://bit.ly/2kSMK4G"><Button variant="contained" color="primary">soundcloud</Button></a>
   </Layout>
 )
 
