2a66d84e113917f36862d83b1b0c4065395bd62c
Walter Cruz <waltercruz@gmail.com>
2019-09-25 00:36:44 -0300

    again

--- component---src-pages-index-js-55a3096e7dc0a983ecfc.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{206:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a),c=t(204),r=t(203);n.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Home"}),l.a.createElement("h1",null,"Hi people"),l.a.createElement("p",null,"Teste."))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-55a3096e7dc0a983ecfc.js.map
